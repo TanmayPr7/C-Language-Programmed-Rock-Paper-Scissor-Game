6fd4acfd2858fa82ea21359d6f7587eb33d76d04
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-01-13 23:22:59 +0530

    Code to Generate Random Numbers

--- Random Number Generator.c
+++ Random Number Generator.c
@@ -3,3 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int main()
+{
+    int num;
+    srand(time(0));
+    num=rand()%100;
+    printf("Random generated is %d\n",num);
+    return 0;
+}
+
+//This is the basic program to generate randon numbers:)
