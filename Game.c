b8c453fc705519b1f650cfe9fa65623c0365c6c5
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-01-14 22:49:11 +0530

    Basic Setup

--- Game.c
+++ Game.c
@@ -3,3 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int setup()
+{
+    int you,opp;
+    int n,w;
+    int x=0;
+    int y=0;
+    int result;
+    printf("                           ***************Welcome to our Rock Paper Sicssor game:)***************\n");
+    printf("                                                 Please play and enjoy it:)\n\n");
+    printf("Set your Target Points:)\n");
+    scanf("%d",&w);
+    printf("                                              Score %d points to win the match:)\n\n",w);
+    while(x!=w && y!=w){
+        srand(time(0));
+        n=rand()%3 +1;
+        if(n==1){
+            opp=1;
+        }
+        else if(n==2){
+            opp=2;
+        }
+        else{
+            opp=3;
+        }
