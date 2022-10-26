1cac38a5616b310a5f4da2068d5620c7b62707d0
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-10-27 00:28:16 +0530

    Source Code

--- Game.c
+++ Game.c
@@ -1,4 +1,4 @@
-# Thsi is the basic source code for this C language programmed Rock, Paper and Scissor game
+# This is the basic source code for this C language programmed Rock, Paper and Scissor game
 
 #include <stdio.h>
 #include <stdlib.h>
